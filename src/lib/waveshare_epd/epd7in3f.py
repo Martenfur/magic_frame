973ca0718ad8ee8440064402b7e0745eeccf4571
Martenfur <chaifoxes@gmail.com>
2023-02-06 21:08:11 +0100

    Removed quantize step.

--- src/lib/waveshare_epd/epd7in3f.py
+++ src/lib/waveshare_epd/epd7in3f.py
@@ -212,7 +212,7 @@ class EPD:
             logger.warning("Invalid image dimensions: %d x %d, expected %d x %d" % (imwidth, imheight, self.width, self.height))
 
         # Convert the soruce image to the 7 colors, dithering if needed
-        image_7color = image_temp.convert("RGB").quantize(palette=pal_image)
+        image_7color = image_temp.convert("RGB")#.quantize(palette=pal_image)
         buf_7color = bytearray(image_7color.tobytes('raw'))
 
         # PIL does not support 4 bit color, so pack the 4 bits of color
