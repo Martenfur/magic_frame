f2faebcb495a39c6c2bd9700e6139443cd36be31
Martenfur <chaifoxes@gmail.com>
2023-02-06 20:39:56 +0100

    Added basic eink drivers and demos.

---
+++ src/clear.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in3f
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    epd = epd7in3f.EPD()
+    logging.info("init")
+    epd.init()
+    epd.Clear()
+    logging.info("Clearing...")
+    epd.Clear()
+    logging.info("Goto Sleep...")
+    epd.sleep()
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd7in3f.epdconfig.module_exit()
+    exit()
