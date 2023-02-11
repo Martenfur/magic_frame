1d0c480e6d7ae906ae3e71b7c25354d1e6e22ba2
Martenfur <chaifoxes@gmail.com>
2023-02-11 23:51:55 +0100

    Fixed cropping.

--- src/image_sources/img_utils.py
+++ src/image_sources/img_utils.py
@@ -16,8 +16,32 @@ def get_ratio(w, h):
 		return 0
 	return w / h
 
-def resize_image(img):
-	pass
+def resize_image(img, new_w, new_h):
+	ratio = new_w / new_h
+
+	img_w, img_h = img.size
+	crop_w = 0
+	crop_h = 0
+	if is_landscape(img_w, img_h):
+			if get_ratio(new_w, new_h) < get_ratio(img_w, img_h):
+				crop_h = img_h
+				crop_w = ratio * crop_h
+			else:
+				crop_w = img_w
+				crop_h = crop_w / ratio
+	else:
+			if get_ratio(new_w, new_h) > get_ratio(img_w, img_h):
+				crop_w = img_w
+				crop_h = crop_w / ratio
+			else:
+				crop_h = img_h
+				crop_w = ratio * crop_h
+	
+	area = ((img_w - crop_w) / 2, (img_h - crop_h) / 2, crop_w + (img_w - crop_w) / 2, crop_h + (img_h - crop_h) / 2)
+	cropped_img = img.resize((new_w, new_h), box=area)
+	return cropped_img
+
+
 
 def download_image(url):
 	img_data = requests.get(url).content
