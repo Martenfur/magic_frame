9f72427662bf6b6c8a0026d03fddb23b28525048
Martenfur <chaifoxes@gmail.com>
2023-02-18 02:54:50 +0100

    fix

--- src/lib/waveshare_epd/epd7in3f.py
+++ src/lib/waveshare_epd/epd7in3f.py
@@ -213,7 +213,7 @@ class EPD:
 
         # Convert the soruce image to the 7 colors, dithering if needed
         image_7color = image_temp.convert("RGB").quantize(palette=pal_image)
-        image_7color.save("mkdir /var/vault/vault/test/rnd/imgtest.bmp")
+        image_7color.save("/var/vault/vault/test/rnd/imgtest.bmp")
         buf_7color = bytearray(image_7color.tobytes('raw'))
 
         # PIL does not support 4 bit color, so pack the 4 bits of color
