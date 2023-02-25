3fd20139c239762a7ecbf699fe503913dbd84cba
Martenfur <chaifoxes@gmail.com>
2023-02-25 17:51:07 +0100

    Fixed indentations.

--- src/clear.py
+++ src/clear.py
@@ -5,7 +5,7 @@ import os
 picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
 if os.path.exists(libdir):
-    sys.path.append(libdir)
+	sys.path.append(libdir)
 
 import logging
 from waveshare_epd import epd7in3f
@@ -16,19 +16,19 @@ import traceback
 logging.basicConfig(level=logging.DEBUG)
 
 try:
-    epd = epd7in3f.EPD()
-    logging.info("init")
-    epd.init()
-    epd.Clear()
-    logging.info("Clearing...")
-    epd.Clear()
-    logging.info("Goto Sleep...")
-    epd.sleep()
-        
+	epd = epd7in3f.EPD()
+	logging.info("init")
+	epd.init()
+	epd.Clear()
+	logging.info("Clearing...")
+	epd.Clear()
+	logging.info("Goto Sleep...")
+	epd.sleep()
+		
 except IOError as e:
-    logging.info(e)
-    
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd7in3f.epdconfig.module_exit()
-    exit()
+	logging.info(e)
+	
+except KeyboardInterrupt:	
+	logging.info("ctrl + c:")
+	epd7in3f.epdconfig.module_exit()
+	exit()
--- src/image.py
+++ src/image.py
@@ -7,7 +7,7 @@ import numpy
 picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
 if os.path.exists(libdir):
-    sys.path.append(libdir)
+	sys.path.append(libdir)
 
 import logging
 from waveshare_epd import epd7in3f
@@ -17,64 +17,24 @@ import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
-
-DESATURATED_PALETTE = [
-    [0, 0, 0],        # Black
-    [255, 255, 255],  # White
-    [0, 255, 0],      # Green
-    [0, 0, 255],      # Blue
-    [255, 0, 0],      # Red
-    [255, 255, 0],    # Yellow
-    [255, 140, 0],    # Orange
-    [255, 255, 255]   # Clear
-]
-
-SATURATED_PALETTE = [
-    [0, 0, 0],        # Black
-    [217, 242, 255],  # White
-    [3, 124, 76],     # Green
-    [27, 46, 198],    # Blue
-    [245, 80, 34],    # Red
-    [255, 255, 68],   # Yellow
-    [239, 121, 44],   # Orange
-    [255, 255, 255]   # Clear
-]
-
-def _palette_blend(saturation, dtype='uint8'):
-    saturation = float(saturation)
-    palette = []
-    for i in range(7):
-        rs, gs, bs = [c * saturation for c in SATURATED_PALETTE[i]]
-        rd, gd, bd = [c * (1.0 - saturation) for c in DESATURATED_PALETTE[i]]
-        if dtype == 'uint8':
-            palette += [int(rs + rd), int(gs + gd), int(bs + bd)]
-        if dtype == 'uint24':
-            palette += [(int(rs + rd) << 16) | (int(gs + gd) << 8) | int(bs + bd)]
-    if dtype == 'uint8':
-        palette += [255, 255, 255]
-    if dtype == 'uint24':
-        palette += [0xffffff]
-    return palette
-
-
 try:
-    epd = epd7in3f.EPD()
-    logging.info("init and clear")
-    epd.init()
-    epd.Clear()
+	epd = epd7in3f.EPD()
+	logging.info("init and clear")
+	epd.init()
+	epd.Clear()
 
-    logging.info("reading image")
-    Himage = Image.open(os.path.join(picdir, 'demo.jpg'))
+	logging.info("reading image")
+	Himage = Image.open(os.path.join(picdir, 'demo.jpg'))
 
-    epd.display(epd.getbuffer(Himage))
+	epd.display(epd.getbuffer(Himage))
 
-    logging.info("Goto Sleep...")
-    epd.sleep()
-    
+	logging.info("Goto Sleep...")
+	epd.sleep()
+	
 except IOError as e:
-    logging.info(e)
-    
+	logging.info(e)
+	
 except KeyboardInterrupt:
-    logging.info("ctrl + c:")
-    epd7in3f.epdconfig.module_exit()
-    exit()
+	logging.info("ctrl + c:")
+	epd7in3f.epdconfig.module_exit()
+	exit()
