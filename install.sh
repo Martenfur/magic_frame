8732ab0885e524d10b9ea4cc2059e5b4614bac52
Martenfur <chaifoxes@gmail.com>
2023-03-02 21:12:48 +0100

    Added daemon reload to installer.

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 echo "Make sure you cloned the repository into /home/pi/magic_frame directory! Your username must be 'pi'!"
 
 sudo cp magic_frame.service /etc/systemd/system/magic_frame.service
+sudo systemctl daemon-reload
+sudo systemctl enable magic_frame.service
+sudo systemctl start magic_frame.service
 
-echo ""
+echo "DONE!"
