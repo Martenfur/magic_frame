3e3b160efd103f65ff50ac805217e293a8f20830
Martenfur <chaifoxes@gmail.com>
2023-03-02 21:09:14 +0100

    Added basic installer script.

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo "Make sure you cloned the repository into /home/pi/magic_frame directory! Your username must be 'pi'!"
+
+sudo cp magic_frame.service /etc/systemd/system/magic_frame.service
+
+echo ""
