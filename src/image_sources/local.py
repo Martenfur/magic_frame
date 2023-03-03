2a078e78e46c1f78da006e35a9f819d635ea9fab
Martenfur <chaifoxes@gmail.com>
2023-03-03 01:13:36 +0100

    Added random file pick for local file source.

--- src/image_sources/local.py
+++ src/image_sources/local.py
@@ -1,5 +1,6 @@
 import os
 from . import img_utils
+from . import duplicates
 import random
 from PIL import Image
 import config
@@ -7,4 +8,15 @@ import config
 class LocalImageSource:
 
 	def get_image(self, is_landscape):
-		return Image.open(os.path.join(config.current.local_images_path, 'img.png'))
+		files = os.listdir(config.current.local_images_path)
+		random.shuffle(files)
+		for file in files
+			if not os.isfile(file):
+				continue
+			full_path = os.path.join(config.current.local_images_path, file)
+			if not config.current.local_allow_repeats:
+				if duplicates.contains(full_path):
+					continue
+				duplicates.add(full_path)
+			return Image.open(full_path)
+		return None
