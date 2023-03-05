e9d694f22d58591b10c19b9194b4787e4de6bcd0
Martenfur <chaifoxes@gmail.com>
2023-03-05 20:47:22 +0100

    Added image saving.

--- src/report.py
+++ src/report.py
@@ -3,11 +3,35 @@ import battery
 import orientation
 import datetime
 import witty.witty as witty
+import config
+import unicodedata
+import re
+import os
 
+def slugify(value, allow_unicode=True):
+	"""
+	Taken from https://github.com/django/django/blob/master/django/utils/text.py
+	Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+	dashes to single dashes. Remove characters that aren't alphanumerics,
+	underscores, or hyphens. Convert to lowercase. Also strip leading and
+	trailing whitespace, dashes, and underscores.
+	"""
+	value = str(value)
+	if allow_unicode:
+		value = unicodedata.normalize('NFKC', value)
+	else:
+		value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+	value = re.sub(r'[^\w\s-]', '', value.lower())
+	return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 def report(art):
+	now = datetime.datetime.now()
+	secs = str(now.timestamp())
+	if not os.path.exists(config.current.pictures_directory):
+		os.makedirs(config.current.pictures_directory)
+	art.original_image.save(config.current.pictures_directory + "/" + slugify(art.image_name) + "_" + secs + ".jpg")
 	try:
-		ilog.log("datetime: " + str(datetime.datetime.now()))
+		ilog.log("datetime: " + str(now))
 		ilog.log("name: " + art.image_name)
 		ilog.log("source: " + art.image_source)
 		ilog.log("url: " + art.image_url)
